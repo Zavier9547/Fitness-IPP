205329a000a7a14bbb7b533acaa25746da4e67a8
zhai wei <zhaiwei@qq.com>
2016-06-07 15:35:33 +0800

    添加了函数包，展示了一些静态的数据

--- FitnessApp/RecordViewController.swift
+++ FitnessApp/RecordViewController.swift
@@ -7,15 +7,62 @@
 //
 
 import UIKit
+import Charts
 
 class RecordViewController: UIViewController {
 
+    @IBOutlet weak var barChartView: BarChartView!
+    
+    var days:[String]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        days = [ "1"," 2"," 3"," 4"," 5",
+                 "6"," 7"," 8"," 9","10",
+                 "11","12","13","14","15",
+                 "16","17","18","19","20",
+                 "21","22","23","24","25",
+                 "26","27","28","29","30","31"]
+        let numbers = [12.0,23,34,42,13,
+                       23,41,19,45,24,
+                       33,11,22,4,23,
+                       1,2,3,4,5,
+                       6,7,8,9,10,
+                       5,4,6,2,7,
+                       12]
+        
+        setChart(days, values: numbers)
+        
+        
     }
 
+    func setChart(dataPoints:[String],values:[Double])
+    {
+        barChartView.noDataText = "you need wo provide data for the chart"
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count
+        {
+            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(yVals: dataEntries,label:"每日训练量")
+        let chartData = BarChartData(xVals: days,dataSet: chartDataSet)
+        barChartView.data = chartData
+        
+        barChartView.descriptionText=""
+        
+        barChartView.xAxis.labelPosition = .Bottom
+        
+        barChartView.animate(xAxisDuration: 0.0, yAxisDuration: 1.5, easingOption: .EaseOutBack)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
